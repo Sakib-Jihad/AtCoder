2c1c522c353195ae276393a2b62345baa2b5e68d
Sakib Chowdhury <sakibchowdhury850@gmail.com>
2025-06-20 22:42:16 +0600

    Add files via upload

---
+++ CodeChef - WATERCONS.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+      int a;
+      scanf("%d",&a);
+      for ( int i=0; i<a; i++){
+      int x=2000;
+      int y;
+      scanf("%d",&y);
+      if(x<=y){
+            printf("YES\n");}
+
+      else {
+            printf("NO\n");
+       } }
+
+
+        return 0;
+}
