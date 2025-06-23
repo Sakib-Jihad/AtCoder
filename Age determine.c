aade948f8cf224ffc599cd5af7bfa0f6b8db8eb2
Sakib Chowdhury <sakibchowdhury850@gmail.com>
2025-06-23 23:17:34 +0600

    Add files via upload

---
+++ Age determine.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main (){
+      int n;
+      scanf("%d",&n);
+
+      for( int i=0; i<n;i++){
+            int x;
+            scanf("%d",&x);
+            int y=10;
+            int z=(x-10);
+            printf("%d\n",z);
+
+      } return 0;
+}
